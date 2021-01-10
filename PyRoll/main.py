f76e5a7052d28b92ec9d461315d24574a308d78b
nSalameh <74309258+NaderSalameh@users.noreply.github.com>
2021-01-09 23:17:21 -0500

    Checking in election data progress

---
+++ PyRoll/main.py
@@ -0,0 +1,36 @@
+import os, csv
+
+# path to the election csv
+csv_path = os.path.join('Resources', 'election_data.csv')
+
+# opening and reading the csv file 
+with open(csv_path, 'r') as csv_file:
+
+    # splitting the data with commas 
+    csv_reader = csv.reader(csv_file, delimiter=',')
+
+    # skipping the header row
+    next(csv_reader)
+
+    # creating my lists 
+    votes = []
+    candidates =[]
+    unique_candidates = []
+    tally = []
+
+    # collecting all votes
+    for row in csv_reader:
+        votes.append(row[2])
+
+    # capturing all unique candidates
+    candidates = set(votes)
+    unique_candidates = list(candidates)
+
+    # setting up my vote counter
+    for count in range(len(unique_candidates)): 
+        tally.append(0)
+
+    print(tally)
+        
+    
+
