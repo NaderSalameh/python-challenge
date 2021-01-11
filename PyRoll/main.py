3339d0f159cd5a63d61bcf1832e7a578996c1ab7
nSalameh <74309258+NaderSalameh@users.noreply.github.com>
2021-01-10 23:32:31 -0500

    Checking in progress

--- PyRoll/main.py
+++ PyRoll/main.py
@@ -17,6 +17,7 @@ with open(csv_path, 'r') as csv_file:
     candidates =[]
     unique_candidates = []
     tally = []
+    
 
     # collecting all votes
     for row in csv_reader:
@@ -30,15 +31,49 @@ with open(csv_path, 'r') as csv_file:
     for count in range(len(unique_candidates)): 
         tally.append(0)
 
+    # counting total votes 
+    totVotes = len(votes)
+
     # time to tally up the votes!
     for count in votes:
         index = unique_candidates.index(count)
         tally[index] += 1
 
-    print(tally)
-        
+    max_votes = max(tally)
 
+# creating the results summary, outputing the results into a text file, as well as the terminal
+output_path = os.path.join("Analysis", "Election_Results.txt")
+with open(output_path, 'w', newline='') as textfile:
+
+    textfile.write("Election Results\n")
+    textfile.write("--.---------------.---------------.----------\n\n")
+
+    print("Election Results")
+    print("--.---------------.---------------.----------")
     
+    # building a list of tuples to create the relationship between candidates and their votes, 
+    # then sorting the tuples in descending order by number of votes
+
+    election_results = sorted(list(zip(tally, unique_candidates)), reverse = True)
+    for index in range(len(election_results)):
+        if election_results[index][0] == max_votes:
+            winner = election_results[index][1]
+        textfile.write(f" {election_results[index][1]}: {(election_results[index][0]/totVotes * 100.000): .3f}% ({election_results[index][0]}) \n")
+        print(f" {election_results[index][1]}: {(election_results[index][0]/totVotes * 100.000): .3f}% ({election_results[index][0]}) ")
+
+    textfile.write("\n--.---------------.---------------.----------\n")
+    textfile.write(f"Winner: {winner}\n")
+    textfile.write("--.---------------.---------------.----------\n")
+
+    print("--.---------------.---------------.----------")
+    print(f"Winner: {winner}")
+    print("--.---------------.---------------.----------")
+
+
+             
+
+
+ 
         
         
     
